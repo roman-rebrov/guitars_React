c4d5da9d87f9ad4b4659c1300b6618050ba25a2e
Roman <rebrovdev@gmail.com>
2020-04-25 01:44:18 +0300

    1.44

--- guitar-store/src/components/Catalog.jsx
+++ guitar-store/src/components/Catalog.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 // import '../catalog/products';
 
 // console.log(catalog[1].name)   
-
+const ttt = ['This is a catalog!']
 function Catalog() {
     return (
         <div className="catalog">
             {
+                ttt[0]
             }
         </div>
-    )
+    );
 }
 export default Catalog;
